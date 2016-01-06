d39769e870b7f4735e177eef186fe6f685c42890
YiQiZhang <jerry_techtree@126.com>
2016-01-06 09:39:06 +0800

    Updated ajax
    Signed-off-by: YiQiZhang <jerry_techtree@126.com>

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,13 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
+var jQ = require("jquery");
 
 module.exports = {
     start: function() {
-        var data = [
-            {id: 1, author: "Pete Hunt", text: "This is one comment"},
-            {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-        ];
-
         var Comment = React.createClass({
            render: function() {
                return (
@@ -35,21 +31,48 @@ module.exports = {
         });
 
         var CommentForm = React.createClass({
+            getInitialState: function() {
+                return {author: "", text: ""};
+            },
             render: function() {
                 return (
-                    <div className="commentForm">
-                        Hello, world! I am a CommentForm.
-                    </div>
+                    <form className="commentForm">
+                        <input type="text" placeholder="Your name" />
+                        <input type="text" placeholder="Say something..." />
+                        <input type="submit" value="Post" />
+                    </form>
                 );
             }
         });
 
         var CommentBox = React.createClass({
+            loadDataFromServer: function() {
+                jQ.ajax({
+                   type: "GET",
+                    url: this.props.url,
+                    dataType: "json",
+                    success: function(data) {
+                        if (data.code == 0) {
+                            this.setState({data: data.data});
+                            console.log(data.message);
+                        }
+                    }.bind(this),
+                    error: function(xhr, status, err) {
+                        console.log(err.toString());
+                    }.bind(this)
+                });
+            },
+            getInitialState: function() {
+                return {data: []};
+            },
+            componentDidMount: function() {
+                this.loadDataFromServer();
+            },
             render: function() {
                 return (
                     <div className="commentBox">
                         <h1>Comments</h1>
-                        <CommentList data={data}/>
+                        <CommentList data={this.state.data}/>
                         <CommentForm/>
                     </div>
                 );
@@ -59,7 +82,7 @@ module.exports = {
 
 
         ReactDOM.render(
-          <CommentBox />,
+          <CommentBox url="/response.json"/>,
           document.getElementById('main-container')
         );
     }
